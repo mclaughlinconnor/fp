86885c82438dd765c8d82c83115fcc271a9428e7
mclaughlin_connor <connor1211.mcl@gmail.com>
2022-02-17 21:31:25 +0000

    feat: many colour service improvements

--- styles/ColourService.ts
+++ styles/ColourService.ts
@@ -1,8 +1,10 @@
-import {Brightness, Colour, Elevation, Style} from './Styles';
+import {Brightness, Colour, ColourMode, Elevation, Style} from './Styles';
 import useColorScheme from '../hooks/useColorScheme';
 import {Component} from 'react';
 import {backgroundColor, Colours} from './Colours';
 import {shadowElevation} from './Elevations';
+
+export const OVER_ALL = 1000;
 
 export class ColourService extends Component<{}, {}> {
   getColourStyle(brightness?: Brightness, colour: Colour = 'surface') {
@@ -26,7 +28,29 @@ export class ColourService extends Component<{}, {}> {
     }
   }
 
-  getViewElevationStyle(elevation: Elevation, brightness: Brightness): Style {
+  getColour(brightness?: Brightness, colour: Colour = 'surface', mode: ColourMode = 'primary') {
+    if (!brightness) {
+      brightness = useColorScheme() as Brightness;
+    }
+
+    const colourObj = Colours[brightness].color[colour];
+
+    return colourObj[mode] || colourObj.primary;
+  }
+
+  getTextColour(brightness?: Brightness, colour: Colour | 'label' = 'surface') {
+    if (!brightness) {
+      brightness = useColorScheme() as Brightness;
+    }
+
+    return Colours[brightness].text[colour];
+  }
+
+  getViewElevationStyle(elevation: Elevation, brightness?: Brightness): Style {
+    if (!brightness) {
+      brightness = useColorScheme() as Brightness;
+    }
+
     return {
       ...shadowElevation[elevation], ...backgroundColor[brightness][elevation],
     }
--- styles/Styles.ts
+++ styles/Styles.ts
@@ -16,3 +16,5 @@ export type Colour = 'primary' | 'secondary' | 'error' | 'surface' | 'background
 export type Brightness = 'light' | 'dark';
 
 export type Elevation = 0 | 1 | 2 | 3 | 4 | 6 | 8 | 12 | 16 | 24;
+
+export type ColourMode = 'light' | 'dark' | 'primary';
