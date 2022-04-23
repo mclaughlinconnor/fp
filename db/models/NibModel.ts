89422f74621ef7de36b5e639afc3fe3612897d39
mclaughlin_connor <connor1211.mcl@gmail.com>
2022-04-23 14:47:22 +0100

    fix: add missing nib sizes

--- db/models/NibModel.ts
+++ db/models/NibModel.ts
@@ -3,7 +3,22 @@ import UUID = Realm.BSON.UUID;
 import {PenModel} from './PenModel';
 import {FileModel} from './FileModel';
 
-const _NibSizes = ['extra fine', 'fine', 'medium', 'broad', '1.1mm stub'] as const;
+const _NibSizes = [
+  '0.7mm stub',
+  '0.9mm stub',
+  '1.1mm stub',
+  '1.5mm stub',
+  '1.9mm stub',
+  '2.5mm stub',
+  '2.9mm stub',
+  'broad',
+  'extra fine',
+  'fine',
+  'flex',
+  'fude',
+  'medium',
+  'mini fude',
+] as const;
 export type NibSize = typeof _NibSizes;
 export const NibSizes = _NibSizes as unknown as NibSize[];
 
