8355d19d523f710aa97792e520c184d962a14366
mclaughlin_connor <connor1211.mcl@gmail.com>
2022-04-24 21:28:56 +0100

    feat: add customisability to icons and add offset for stacking FABs on top of each other

--- components/Styling/FloatingActionButton.tsx
+++ components/Styling/FloatingActionButton.tsx
@@ -1,8 +1,9 @@
 import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {ColourService} from '../../styles/ColourService';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {ComponentProps} from 'react';
 
-export function FloatingActionButton({onPress}: {onPress: () => any}) {
+export function FloatingActionButton({onPress, index = 0, icon = 'plus'}: {onPress: () => any, index?: number, icon?: ComponentProps<typeof MaterialCommunityIcons>['name']}) {
   const colourService = new ColourService({});
 
   const styles = StyleSheet.create({
@@ -11,7 +12,7 @@ export function FloatingActionButton({onPress}: {onPress: () => any}) {
       width: 56,
       height: 56,
       borderRadius: 28,
-      bottom: 16,
+      bottom: 16 + (64 * index),
       right: 16,
       alignItems: 'center',
       justifyContent: 'center',
@@ -26,7 +27,7 @@ export function FloatingActionButton({onPress}: {onPress: () => any}) {
 
   return <View style={styles.fab}>
     <TouchableOpacity onPress={onPress}>
-      <MaterialCommunityIcons style={styles.icon} size={styles.icon.width} name={'plus'}/>
+      <MaterialCommunityIcons style={styles.icon} size={styles.icon.width} name={icon}/>
     </TouchableOpacity>
   </View>
 }
